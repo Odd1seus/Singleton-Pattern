e3ec7a9181c7e35fd42e42995b42d9f29a25b4f3
Odd1seus <125728600+Odd1seus@users.noreply.github.com>
2024-11-27 23:47:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.Instance;
+            var singleton1 = Singleton.Instance;
+
+            var hashSingleton = singleton.GetHashCode();
+            var hashSingleton1 = singleton1.GetHashCode();
+
+            double latKharkiv = 49.988358;
+            double lonKharkiv = 36.232845;
+
+            singleton.CalculateDistance("Kharkiv", latKharkiv, lonKharkiv);
+
+            double latOdessa = 46.482952;
+            double lonOdessa = 30.712481;
+
+            singleton.CalculateDistance("Odessa", latOdessa, lonOdessa);
+
+            double wrongLat = 1231.4141412;
+            double сorrectLon = -12.0123901;
+
+            singleton.CalculateDistance("Not existent city", wrongLat, сorrectLon);
+
+            double correctLat = 14.4141412;
+            double wrongLon = -12123.0123901;
+
+            singleton.CalculateDistance("Not existent city", correctLat, wrongLon);
+
+            Console.WriteLine($"Hash code of singleton: {hashSingleton}");
+            Console.WriteLine($"Hash code of sinleton 1: {hashSingleton1}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternProject
+{
+    public sealed class Singleton // "sealed" means we cant make inheritance what is the main point of Singleton
+    {
+
+        private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+
+        private const double _KyivLat = 50.450001;
+        private const double _KyivLon = 30.523333;
+        private const double _EarthRadius = 6371.0;
+        
+
+        private Singleton() 
+        {
+        
+        }
+        
+        public static Singleton Instance => _instance.Value;
+
+        private static double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180.0;
+        }
+
+        public double CalculateDistance(string CityInput, double LatInput, double LonInput)
+        {
+            try
+            {
+                
+                if (LatInput < -90.0 || LatInput > 90.0)
+                {
+                    throw new ArgumentException("Latitude can`t be out of [-90;90] range");
+                }
+
+                if (LonInput < -180.0 || LonInput > 180.0)
+                {
+                    throw new ArgumentException("Longitude can`t be out of [-180;180] range");
+                }
+
+                
+                double kyivLatRad = DegreesToRadians(_KyivLat);
+                double kyivLonRad = DegreesToRadians(_KyivLon);
+
+                double latInputRad = DegreesToRadians(LatInput);
+                double lonInputRad = DegreesToRadians(LonInput);
+
+                
+                double deltaLat = latInputRad - kyivLatRad;
+                double deltaLon = lonInputRad - kyivLonRad;
+
+                // Haversine formula 
+                double a = Math.Pow(Math.Sin(deltaLat / 2), 2) +
+                    Math.Cos(kyivLatRad) * Math.Cos(latInputRad) *
+                    Math.Pow(Math.Sin(deltaLon / 2), 2);
+
+                double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+                double distance = _EarthRadius * c;
+
+                
+                Console.WriteLine($"Distance between Kyiv and {CityInput}({LatInput},{LonInput}) is {distance} km");
+
+                return distance;
+            }
+            catch (ArgumentException ex)
+            {
+                
+                Console.WriteLine($"Error: {ex.Message}. City: {CityInput}, Lat: {LatInput}, Lon: {LonInput}");
+                return 0; 
+            }
+            catch (Exception ex)
+            {
+              
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                return 0; 
+            }
+        }
+    }
+}
